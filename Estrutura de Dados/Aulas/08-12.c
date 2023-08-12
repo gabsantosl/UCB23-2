15cbbc5bb09b54c0991b0f37fe1b8120fb3ae7e4
Gabriela <gabsantosleite@gmail.com>
2023-08-12 10:24:30 -0300

    .

--- Estrutura de Dados/Aulas/08-12.c
+++ Estrutura de Dados/Aulas/08-12.c
@@ -1,8 +1,16 @@
+/*
+typedef struct Aluno {
+    char nome[20];
+    int matricula;
+    float nota;
+} Aluno;
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Aluno{
+typedef struct Aluno {
     char nome[20];
     int matricula;
     float nota;
@@ -14,15 +22,17 @@ int main(){
     int i;
 
     for(i =0;i<3;i++){
+        printf("Preencha as informacoes do Aluno: \n");
         scanf("%d", &alunos[i].matricula);
         scanf("%f", &alunos[i].nota);
         scanf("%s", &alunos[i].nome);
     }
 
     for(i =0;i<3;i++){
-        printf("%d", alunos[i].matricula);
-        printf("%f", alunos[i].nota);
-        printf("%s", alunos[i].nome);
+        printf("\nInformacoes do Aluno %d:\n", i);
+        printf("%d\n", alunos[i].matricula);
+        printf("%f\n", alunos[i].nota);
+        printf("%s\n", alunos[i].nome);
     }
 
 return 0;
